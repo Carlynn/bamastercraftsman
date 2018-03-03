9c462fb0699ff4d628b5009845fd490e81255ca1
Carlynn <chick04sd@gmail.com>
2018-03-02 23:22:59 -0800

    services update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,12 @@
-let projects = [
+let services = [
   {
-    "title": "Heli & Carlynn Designs",
-    "description": "Branding website for the passionate business owners to solve their branding needs.",
-    "created": "October 2017",
-    "image": "assets/imgs/branding.png",
-    "platforms": "Ruby on Rails, JQuery, HTML & CSS, Materialize",
-    "gems": "Action Mailer, AWS, Devise, Figaro, Friendly Id, Paperclip",
-    "link": "https://powerful-stream-25184.herokuapp.com/",
-    "github_link": "https://github.com/Carlynn/Branding-Site"
+    "title-1": "Patio Furniture",
+    "description-1": "Whether you want something simple to compliment your existing indoor or outdoor area, or a unique design that will be the envy of your neighbors, we’ll be able to design something that is both beautiful and functional and  ts seamlessly in your home or garden. Unlike inexpensive wood or plastic patio furniture our well-built furniture and gates will last for years to come. The quality of the  nished product speaks for itself and our customers are always delighted with the end result.",
+    "photo-1": "assets/images/services-cover-patio.jpg",
+    "title-2": "STAINLESS STEEL AND ALUMINUM WELDING",
+    "description-2": "With more than 30 years of experience with stainless steel and aluminum fabrication in the heart of wine country, we o er custom gate designs that will enhance your home. We o er a full package suite using high precision tools to do the job. Call us today for a quote.",
+    "photo-2": "assets/images/services-cover-stainless.jpg",
   },
-  {
-    "title": "Viz Data",
-    "description": "A website dedicated to users in the Web Development field, searching for career opportunities that can be represented in graphical formatted and compared.",
-    "created": "October 2017",
-    "image": "assets/imgs/vizData.png",
-    "platforms": "Ruby on Rails, JQuery, HTML & CSS, Materialize",
-    "gems": "Chartkick, Devise, Friendly ID, Kaminari",
-    "link": "https://ancient-reaches-44992.herokuapp.com/",
-    "github_link": "https://github.com/Carlynn/viz"
-  },
-  {
-    "title": "Travel With Us",
-    "description": "Travel app to show all the must see attractions for Europe within the following destinations: Barcelona, London, Paris, and St.Petersburg. The main functionality of this application is to help users create travel posts which consists of personal recommendations, along with reviews of things to do while in that designated city.",
-    "created": "September 2017",
-    "image": "assets/imgs/vagabond.png",
-    "platforms": "Ruby on Rails, JQuery, HTML & CSS, Materialize",
-    "gems": "Bcrypt, Devise, Friendly ID, Kaminari",
-    "link": "https://guarded-taiga-12222.herokuapp.com/",
-    "github_link": "https://github.com/Carlynn/vagabond"
-  },
-  {
-    "title": "Pub Hub",
-    "description": "Living or working in San Francisco? Eating out, but don't know where to eat your lunch? Pub Hub is the place to go to locate the nearest Privately Owned Public Outdoor Space near you!",
-    "created": "August 2017",
-    "image": "assets/imgs/pubHub.png",
-    "platforms": "JQuery, Body Parser, MongoDB, Mongoose, Express, Google Maps API, HTML & CSS, Bootstrap",
-    "link": "https://sheltered-hollows-61938.herokuapp.com/",
-    "github_link": "https://github.com/Carlynn/PubHub-Proj-1"
-  }
 ];
 
 $(document).ready(function(){
@@ -53,8 +22,8 @@ $(document).ready(function(){
   $(".button-collapse").sideNav();
   $('.slider').slider();
 
-    $.each(projects, function (key, data) {
-      $("#card-container").append('<figure><div class="card"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + data['image'] + '"></div><div class="card-reveal"><span class="card-title gold">' + data['title'] + '<i class="material-icons right">close</i></span><p>' + data['description'] + '</p><p><b>Platforms: </b>' + data['platforms'] + '</p><p><b>Gems: </b>' + data['gems'] + '</p><p><b>Website: </b><a href="' + data['link'] + '">' + data['link'] + '</a></p><p><b>Github: </b><a href="' + data['github_link'] + '">' + data['github_link'] + '</a></p></div></figure>');
-      });
+    // $.each(services, function (key, data) {
+    //   $("#services").append('<figure><div class="container"><div class="row"><div class="col s12"><h2>' + data['title-1'] + '</h2></div></div><div class="row"><div class="col s12 m6"><img class="services-image" src="' + data['photo-1'] + '"></div><div class="col s12 m6"><p>' + data['description-1'] + '</p></div></div></div><div class="container"><div class="row"><div class="col s12"><h2>' + data['title-2'] + '</h2></div></div><div class="row"><div class="col s12 m6"><p>' + data['description-2'] + '</p></div><div class="col s12 m6"><img class="services-image" src="' + data['photo-2'] + '"></div></div></div></figure>');
+    //   });
 
   });
