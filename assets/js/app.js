55c3a3ece16bbc2b0aa3b4f0e27298aa28c90df4
Carlynn <chick04sd@gmail.com>
2018-03-12 22:11:19 -0700

    mobile responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,97 +1,55 @@
 let galleryPatio = [
   {
-    "photo": "assets/images/services-cover-patio.jpg",
+    "photo": "https://lh3.googleusercontent.com/IGwBnlHn-veonh35EFiklPm1gkuVH_rwfVGZb0B8BactKOsOXmtb1sEjsvb9_NmOuNAAQ9BlpHMuk3Q7m9UEvDYhDf3bS7jxeOevzcWIW2HBy0QAb01IOMyyKjwqvthuejEphDvSGN9yKhDYFGDoXuGrmy86_9RA7v7SWc8G3kNPeq2QSDDDX5dadWmM8p8fD08bhTEYO5uq6E1FVxSTkjz5atZx-CFzF0FbuxXvMMA9dH9va7TcE_00mQjsEYGM6t7x4rCqd8eST-75zr57J7r25laIsTn6uX7VSZPug1aSnCLzng2rJSleu19KZeQ_z0eo3pZyzI2K2n4VHl_y3esyWy-9OIl1uRys-1uzAPjlqfHbRFeWBbbdtd0kYsaaN-11ifIrPRlZnTyCwkxgcQw0PYzpYShzOV-aJCLrQ1yi9KYR6v4kpdXscEdGOoxnWoTX5zFfACg0EXUH5mv-zQBgkMOJ7Rk5WyG7vfhVTWTTVofEex-Xa5op84yBQCulihvcJlhv3z7ujqRT1mLMI4z4O7unifH5itTNZYobLuAjVjdh6-K0T-iu-DUHGlkKliWxbl67WdlkEpflY1FYSq58s2PZdTnMmQ43XQ=w600-h448-no",
   },{
-    "photo": "assets/images/services-cover-patio.jpg",
+    "photo": "https://lh3.googleusercontent.com/yWGXPtqlVwst8sOKZjAg9VsOw1pCFLJpKzg9kwUilU8z-Hgits11jgUVzaujXIajKmOZOeKSQA5nCB7jEMyaafRBsMBQt92NylXdU0oDrGrorKiAjlCSOykvWJjRTjaSM0RxhEWq7tOfZ9Xl-kDUojmGsw3rP61BfzHvqSgZlOLsObs33-vaxTTofuhUkFjGYgf8Sq__YU740VgqSuzftwDlH_XWUaaX3p4T8xfsIcLFNNYofKL3wG3E56H8dcZnYw2VEcuZupOX5Wu5D9QjJLQk9sczo5PZpJbCtdn6TXkPCmowg2hlzUEE_BoJNr6M9vQBoOsLockj3bQOWx7fCfmWoaCZmsSDKxpGeUtYCXySbw5cLfIOO_dBo4lVrBbKKJewbtbv72LD-y6lqy_VMjhHv9C_jG9ZtGC5YLnqj7Yhdy-GGbCFFrs1bNHkh4NfM67ZmoQ4ZPX66DX8Kbvc_Rq9YpFs7pKA2Wf3_r8i_FtBWpOFwGpRH8HR7L2IaLZA4kialIEL6EwuJKpQzlAYaVBpQQThCC3ovCEuLXzXeo2G3ohZMilm-805QBt36B7xKQO9gX7DNCQkVcYT8H9BmbzyD3Fqpca6eUUe9w=w343-h450-no",
   },{
-    "photo": "assets/images/services-cover-patio.jpg",
-  },{
-    "photo": "assets/images/services-cover-patio.jpg",
-  },{
-    "photo": "assets/images/services-cover-patio.jpg",
-  },{
-    "photo": "assets/images/services-cover-patio.jpg",
-  },{
-    "photo": "assets/images/services-cover-patio.jpg",
-  },{
-    "photo": "assets/images/services-cover-patio.jpg",
-  },
+    "photo": "https://lh3.googleusercontent.com/plkp-xhX67ZssRkJeMT5az7e1T-sDGUVOHNPdaz6DnJDyCawRPBRsDQRFdNk8Qoj1HOcL3Mp5q6n5t7kleTd7qwjhSRv7gTEKPPwSrzA3Sv2fJ1JCTZtS8glgsxnt_YQyFUaWRRD3qn76Pz8QnoUUxwXEtdQYU3VbWTe21CxSys1da973boFFJXuZhrgz6S-ApULTfGQ-Dixswc_cCzv1ITxXoX_s_vQPqxpNEnAZ8UTHAYWQDqhNxmYpes0TZD6IfU67Yq8zVSOJYNaWdzi6Nf3PTdHxBsAwgKNAyD8rSjuQDrPANxJYTTWEQhmzotZ843cUUuPnC5IiIWB5ap12f_8hQcjQobD1MnKXeeGp-wFnb6YSFVknibMPkZDVVOzcOgZEVog7vxSwosBSodDjkpxaSH7ygz14PjL8C2faigvU-8YLZ3l811GFPBOil-bLh4Tgqv0O63J58NKYN0ryQboyFaf7tajzqcebUtq1_gnEtYd5LDisHYNIJjcAOuuiXO5JG1oK3DuBZ_0pqdl5dcNbInMKyY2e5nak3c5AuNp_WzePnNcmY6kHtDUYPZKXH6YQ5zRyVcnDHtPVzErTSUc6J_yAWjeFFbk-g=w600-h437-no",
+  }
 ];
 let galleryStainless = [
   {
-    "photo": "assets/images/services-cover-stainless.jpg",
+    "photo": "https://lh3.googleusercontent.com/pyF_ogq_IwOUJ7Q5AgIs2WRtdVn2JYYZbULIMsmESMpQ2TV6eSdIx0N1AMSyTIGEvc0IAhHYBi0k_-t3G0cvEPzyeN9eeCiZol528agYnFBK_JxRtg_p2mMGRgULqaW4JXxC-WF0mLrxw2jum4gmHMwA2TkjEqRTk2Hr5fPMnOx-8_cr29RvP2bSfLH1g4CGUiTzCUqclxPceC8BiklI9ERHXmaKJ7Rn-EZGyd7Na8Dr7qiN4bdFJs48x7jf9unPrpRMA7nr3ULs2A_dIlrYlDQYoyguelaq8evi0bghAnCPtlDKYENH8BgAKKvdQuzu4ROqyA0OfhYMyB9ver9yx9O4OLS3hhm4m2nYmEfjZ61Wm-396oQ7UKhVRCrwyecFBGAdIKTqHJPWvk0UX0RcbupF3O_LQSfgiMbEp9KGkI218crB6rYtx5nSD1tTkoZTleDyRM1YWv2UJMh2KXkd7yaVvguMM5r4X6baokptvFdV917hyx0HO2NI1OXjj-iZeEa0DgDNl7WVVnW9GI030KWySdK9Wt6fOvAF0o2k3P3ogJeflQfMOEP1ljhmBdbl1AWMuISoIjHN534b8UABp1T2r2e2wMS5h54kVg=s200-no",
   },{
-    "photo": "assets/images/services-cover-stainless.jpg",
+    "photo": "https://lh3.googleusercontent.com/4D_gq7Ac1HQ5WgHKjMBGVueC0DaAKL7oDAL_fq3a4ikLRMJQGv3pAxHDi6ePacX8jjBZJ5sKAZutxsoprjhrekSLSmGT2TXAH-7R7mBpfRBAN_4qDREPLXD6YjuYpXrsrS9NgxjpxOTKkns5Mz4eO6oy8YLOjQUQbigR8c3_GCHBiaG16fNVkW2RUWR5dLaPsPWrCzfEXKpk-r085D0O9lXLoRKXZN3GyE43R5t7sKntZ98PQBw7dnquOVxhPs9q1Rekg8-Z4S9fOeit0RnURnxs1Q4Fehtz6_ZjmP5Byi4sCGbnMt7pV524cgymozWPQhHK5_xuv8K6KXn2ZbPMNOBsPTcDJVbfbgt4-RaDwRt_T7Ydim5eCJ7QGSDi21OunC5_EvsnjKDmsRZLU4VfEg4da45OmqnqiPNXrer0zJDZservEXf_9Q4PzAf2jVhNoSKnP1fNpWVCJLgh45E5dB2yV0PKjyoH_rxYQ1SkfytcFKvTaIYl5c-oekQHpOdBL5KWVUatrB5oC82-IE-tdwjcaXRQu8ctKRsA4TMD4e7NPPNahfQCUb-xOODgpuUG2j-iCVORfsg9y7t95rVrVi6B8f6xA_7joX03Kg=s200-no",
   },{
-    "photo": "assets/images/services-cover-stainless.jpg",
+    "photo": "https://lh3.googleusercontent.com/pU-P1UD7Z9ETnrUTb_8-1nmrwcdWXcEiaadFKMNlGHsgOJdF9XzUZo5ujMqMPO7NJW9q63PU4pJDs3i84WJ-rkmzjXwVTs0KVjzNHyaPubORpssvvOTkWQcFxDKPZQMFG4uVP8k6LbQeqGdLxhVhRUgtWoamkXqh16GBKe3sYB3-kUu-70Vh0_wWG7Zq3Nq54xkMy-qNVW0yKmH9UD6RW45c9Zr_sVl14-DvtZheK3zPl1i_RNUh-uuGZApaKwJRSnbrAZJiypYCbXmx0wNnoMgUisw8sSAIuuJXlacAWZGGVw6UWZMTHczSLWLJOuKRtqoXd5t8hpTWeLYaaOhgx6Lfl_H3HpFnFE0rJYgGI6TcIcfLVWyF34qPZQEXcNVPmctRdErFfpxjDe0VuhizCAgRfg70HYEr5dJTpSKa2OxekWhJD08ZaPDGk_xem9BhJQcg36Xjd5J7gMBviccdDcnwzaS0hDzgLK2E8-zqBPiV_ORZd9ElabH2UTuFsl3tHxtoN8_hPyaka1CYT4dg2b_gEcwAKvorrQfsFuUEQcXfCpP4207TLyek9dGHyeS-1-pgDWmLPAIk_8WpUx8uz3XEgWUHwu5DD6xO3A=s200-no",
   },{
-    "photo": "assets/images/services-cover-stainless.jpg",
-  },{
-    "photo": "assets/images/services-cover-stainless.jpg",
-  },{
-    "photo": "assets/images/services-cover-stainless.jpg",
-  },{
-    "photo": "assets/images/services-cover-stainless.jpg",
-  },{
-    "photo": "assets/images/services-cover-stainless.jpg",
-  },
+    "photo": "https://lh3.googleusercontent.com/epvi701XrPWSirSq_CjgFV8rnDUAZK_2cvOZmp0fVoFCTBWYClipkEMpWYVLLPluC2jsCgiWuoPiyVyLVdHCHgdESZ6lX2YzT4fw5h8PNUoohiSdh-PAc07E0ksz0jnCPgyG3uAbls7oEM3b6BEf72aKIdFZFc4atcNf-Wf6jBmXBENNWjfNktUsPA4PBewu3YR7-8Nvs2hjHwBhgHDNnNaxO-rZBK6jO7pfS9mKXekwZ4M6YTJKNq-Iw2UsR2odTKhdX-46hhqYCn3iGKPvgyEipcbOnpy3UVDdOIYS2mtrw8E8uTp0kYC2GMOxuCvSEBdkFyR_znyEwSBvZVR3r0SSGOZEK064C7qXLBbREhQKksvZT1saQWiOwP8KGyITa28tOPes9eCZZ8S7rJmSL7C0kc9jylIkAQ6nkodsf7OiRemAWJujqn5d2TiWnbwoYFA01hj0aZYGikMz5WzgFTLkVB90BW_7xKcIOZdx9E3WPQqfopMLpoZagE87zc2fB4d7Vr4gqRyYNNhiap5UHv3gBa8-1kQE2JsyXrYHkQ21J_VfrBlQzrWx5THOAfZNZuRKSNSrjT5Blum3kJR8chpeeJh6h5qJn241gg=s200-no",
+  }
 ];
 let galleryFarmhouse = [
   {
-    "photo": "assets/images/services-cover-farmhouse.png",
+    "photo": "https://lh3.googleusercontent.com/V5Bm1AMfSzdva3Nr2OXBQ05K1szsOnxDWQM12YHMIWqJnKSrFQMb7tqNmpZd25wTRFSJru7dc6VEtwd0Fq01NWx_3VhhhlcSUj2NPMSaTUTSQjjonfA0yIuKIUB6Mndy3xVns1-GRd3yo7hCIg4kg3jRxqNHXuDtea653JdkQU61FvZJ6KYD2ejRqjenuDA4S5fK6UIDWjhmY2X6d9CGQ0P9GUA1yA5TU0t3QRgH2Pz3ortgSfnUzoouSL_F5n5ZHUY7XaUtSDFD1tTcJybWLS-mhDFSDDRgCwjt7ha6PmbdRnNpJSSiZyHBwo9m1tJqWQHggsmN2NfbOlcWmcWp5BOiSiXf2Qc3M-CdjPkKfrj7GXaCzKQ5Nr0_EkNEs-Yh2dlwNCceke-FA1CRRhghcOKoYQSMI7JkNx9UHdHZYZMABbOtBPu5iwUCm8BpRN1vw6CwEDAxvai87WwwJzyT7Ksb0gwKWIj3Yi190nkUumybR_o2xG4pHOE7lrswEGAuq0Hc_zV_aWi9uRuOAu0_9Jii7e4Bu25gmTv3tLrxMWDuWeMcCPYYw6JdwT8JiPKAe4KeWpa4B6BKjDCONZJQ-pwurWKZvtcwDJE6sg=w253-h450-no",
   },{
-    "photo": "assets/images/services-cover-farmhouse.png",
+    "photo": "https://lh3.googleusercontent.com/acLJvS-3cP4LbWAMzwVRNtqLKLXJg9r7CJYWAUUdNQU5dAtbYFtRmdZo3PR490JAOtCIMLkvN06Phn3DgAj7aWExcAW0pHDVxbPLcvSJ0uDmoNh2_wrP3CwI4zUyu42fhbi_KAYWJdWBG9M0zDAZ7aLmNT2dBj-z_8nxW4uAS3KM1cZt4YQbTANJL5DEBZPTXONDGJpMt1n_0EFjBF9y1O72nr1VuJpk9y8nyp7c4DNv01cEyv045lidef-F3rdgLaTdz1b-mboXcHpI7FQHF7ONPqDijh6EfNcna638pAoevcogBa3B-KWG95v21t_s3w9JJpAbDk2KG3zRr8wpVOV5gqC2kaaWl61LEkmqvpGiv_rY5lq1yt3bJkfHONmeg7v0hZxoyQwdJTOklSpgTSPUj3FobnHcLZMdEQQUGjJEsGKeXnZ3NQ2Q1K4Y74N5brZI0Ox5rLm1QRj0udxiCYoaw54aVd4I73MZ7V9AF51xHMIXx9c-TJ4vkqMGqfWIWitUIJZbW6T8Xk6hq0fbcqRI5LCyiO1ahoYemnHEC7TNTDW4kWmZscefBhSLE1rUw3Rt3n1w-QhuiJx53xd-hTAKdsKnBMCBsOPbrA=s200-no",
   },{
-    "photo": "assets/images/services-cover-farmhouse.png",
-  },{
-    "photo": "assets/images/services-cover-farmhouse.png",
-  },{
-    "photo": "assets/images/services-cover-farmhouse.png",
-  },{
-    "photo": "assets/images/services-cover-farmhouse.png",
-  },{
-    "photo": "assets/images/services-cover-farmhouse.png",
-  },{
-    "photo": "assets/images/services-cover-farmhouse.png",
-  },
+    "photo": "https://lh3.googleusercontent.com/OFm6ob_0hRU_WvB89WXifzkpVkL2fyMZI-elvMRhjH8mlgLnTTQpiJnWjDGoR39PVnTZL9V_63WUepAjGuDzlMsT7qACeQugrlpXEvTrD7J2pSNm4LS4il98q1VH7GYTGbvltKUS8GEQo-Gx9WB19lk9wqzPi76890F9MNNHdOMbwfyfX8dbKFWxWb4GvRi5nALCyiYjyc9R7VAgGM5FNWmq_0nl73omlbiFg7CSTtWiIZuLAPYJXs48sCPYOAt41E8kv23qDN9571iC6TnYaVUEzxO2ARLS2vY07B4HvqLYYGKs-DgQchsgh9Q8xedOGwQgV04EfHc5W8G7BINsbI_44cp-HHUUR2K4Nxn2hECtwMhGsv-vd5_pOm9KErXXwAYS8oq3xDmr8PVpq8qqub1juHvOW5qrJ-S52AHr1IC3KZb5fkIWw7nRdyJNsR6Wu6Q31XqHoh6EP0vcgFz837GQGX9RDMXYd09ubgWKiPXsqobRi9f9JMyOP8KWTjlfliQRTqpoNPC5M0xhwG4jfe4n9Ota1f0JkUw5H56W_apb_nMA86nTefwChyTPjb0a_1KGLamhMbYTBEd5YwxpiGNoZORbOXSxxanxHw=w600-h450-no",
+  }
 ];
 let galleryNatural = [
   {
-    "photo": "assets/images/services-cover-natural-edge.png",
+    "photo": "https://lh3.googleusercontent.com/VxOqJkslUxX3FhYvl8V8xdF8P_JBmFVhSLDeZWJQDqtDlbdYBPqj-H3ysvFgmSTr258vLnU6_NiPE7Zqx8xtUEFWYnuqk7U0V2sIO2MQQvcAZPAH3cy4aLqupEoXAAE2E7o9UwZLVy-uj4K-4mGdNXy3EAftZJoU7hNwU3qdry4kJn4uigXo4iLYa0_5qoeBLydWolJeThA5yYvFdGWLNmxtVx-BavRYi9F5-gA1FVNLedMsLote32epmiRHMF2U4uQCnvFtROB2HGII9ASaf_mH27DBzEKEOfy6LvZpsK8e2Dqj_kKTXIA8RPcWi8a1SN1xP6LoTH6wwPgsyoqSMkPKJnenChCEIhRqC--6zYHBeOwfblN7mdTAcokbl7WScMyjiLYhoSG6w_KIxFdTRab2LX_QwFStJjU0-NZn-PPMV4UStqmgOhofvfKqEdoVWhWvWtjVOq_d6r6pvNZRpD3YoENRJ8y7VMr8k3KF7bzWHgnuLbIxcXb04npPC6_QTY8G205z8vwU2FWe2zFUKWiSOC8nb_w_5PQKCDiba3UNzmfSR2MXl3LsgatJknxnXRbjBPLufSmARXkTrEM_GUgFMY3XQzxhArxcFA=s200-no",
   },{
-    "photo": "assets/images/services-cover-natural-edge.png",
+    "photo": "https://lh3.googleusercontent.com/aFoDYMopfLD5yk6H9FNxiuiyv6V2gSUAdDvtMzmYaDWJwDWXKs6rXtFcAzADDZ8kgCfnbUKH7e61-ICQr-plhdzn_n3XOtFYM4sDNOAUOdeCFJ7DO0gXSCxTiaHu_cC7GjmQYaAwlMGPmi0sASpVTjEklyHixpSKDTl7MKvfISCBJBcgDJiqTjtOFtdNJxvbq2gpahFTkdnYMg9ZBS8QOyxWICxmIJG66MNrLSZA9X8HBco_JbLSbSFBs1JMAShxozt8NJMxp7IKdDZjW9qGNYdIAOQEAB8XiV3Le1-VjLWQoPQHaFyuNKsNn0NsRcWV97J0ddpbPIq85IkAKN6V7c-bojf7I597KkIuIX5XduiQ-BeCa-Hrh6OTyfs6iQvDnTVTiJdAB-Zpg8GKEIWePs2idesKMcZ3vwrzP687vGpKglQ24Oa9vFa0mey4zaqSZgbJJiFMHWWSqt1OhA6hZkCFGRlYuUZNZGOzlkZguk8BuqUcnXYs9nLCymZfwYMA0sNdxRIJ7M5rVal9yQzMZOO9GJbo-56s9jyqFVb7GSuliSvYg4jHXXNuYzjhFW4T9XvNR8s5lh-Fxp4xv9q164cZ_Bw74lUXs9Cllg=s200-no",
   },{
-    "photo": "assets/images/services-cover-natural-edge.png",
+    "photo": "https://lh3.googleusercontent.com/fIjy8I3f1j0o1UVFPWEm5q4rnfMM3gP2Jbl-G4_G6cJK_B34YHp-7CWYFJvQrt5vPbYY15qHdJV7tfCit4LgCWKO92bqVS0q9i9OiI2CXZzqX5Pen7eXonKKt1O3v1v-9pMhssE24GqYepxiovHJdL-J1yEz-xxsJ6NOiIKG5poJwdO1vjymp8QhNuoZk5Lhsi7BoZIyoiynQig7pGgvyDxX6eqEiLMk0vt3-i16dvajQ4EL_Pev14ctmUOe6Vbxd5NWW4-auN55bRjam9E35gn1RyKc_sd0h4pyM85gqDmioEed-ktXVglgw6mePNH6FXz4zDq-h7CrX8d_3DiXa-3c7rwr1Gk21YbgFcLPAeRZ4qa834jgsTnpgDrP-56Sa5zNLJXPwz_l5uYrbk72ZdvCBFnKlUByzjyl9BV1hOFSE01h-stk3ic_Ln5kR0pQIgYcSJEdckwz2tLEnp-G97s14rQ2mOxZtAOj0EGSabmGq1QDmt35Xpe8v0kVN5yhVUuftUn2_33Sp0gBhJY3mPxKr1GzbbS9wsLqpjfgzQHC_p9hrXU5hQuNXayQhmbZLgDFoKlDYMjKyt0c_rDODpLlGo1CR2DC0l98uQ=s200-no",
   },{
-    "photo": "assets/images/services-cover-natural-edge.png",
-  },{
-    "photo": "assets/images/services-cover-natural-edge.png",
-  },{
-    "photo": "assets/images/services-cover-natural-edge.png",
-  },{
-    "photo": "assets/images/services-cover-natural-edge.png",
-  },{
-    "photo": "assets/images/services-cover-natural-edge.png",
-  },
+    "photo": "https://lh3.googleusercontent.com/ZGImaExtf7jntMt3AD-JS28hiIw74j5mJjns1tg11F8x3GC9BymQYKmOkDfgI9-2x-u3LRWm089tjopFtNMPGu-gDR2bS9R0VUcYR90GUVN86Rg4jxLyQUCY89QIt52gafP-2AYJqyXKMwwluuHRM7ivSBW9NbHGzjKF7zIzKJZ5tcdb2mA1it41Nrg4IisWyiY2RuDa2_p3r72a7_GvSW-OUC1n0nTIOCEQmbZFUD-HerudpFne8hvK_vFmjPQbKJEWthZzN1MxRlfpJ9nHt8h9QYyjUs6R7F_QwKg1ZOlVQSyisktVkZUSRw3lDApXHKMGb9L8vlKwdIeRCZ-z2mrKdEVsqCR9NhdWUwwNuVN-imT3E0JHLxELFxgj_6nzm6cNAX1VagYIKR5XDHiKKX-uCplqYQkADGaSGeNz4wEPiXcv8wrprs6RhWKFwHBRotVemUiMW6Ajurp4bUMlHEfVl07FWn36QyKKwpwH3dlynR4vPDMQB8Jxvv-W-zp8uK7WjAt6ydTpqN6UUAjbtWqCW03cCiI4b1Qajb3yeI-gD_mW-FyPYah3ck6dGY_KcYSy6AnU-NAOGfXXbXOyGDZuTjIbvaB4ZDvd4w=w582-h436-no",
+  }
 ];
 let galleryCustom = [
   {
-    "photo": "assets/images/services-cover-custom-woodwork.png",
+    "photo": "https://lh3.googleusercontent.com/VRNfgSyDgyv9w_Vd21k9iYO33BCw07Ek7UK4_OvBX3MsO1_h1cvhYg2TFgauW5CM1JSF4eNMCvvEjuEJm3c-BmE-TSmc7oky6pgRZOTwlbBSxijKe6R2BUHAkqMOW4JF_1IV8s5n9y7Vn2EPZzEL9pwG21NaGQICZfWZ1G1rWuz9majau3NmGJEvHdGVh-AZbch0Xbb0tK9HZvulAsu_9FAm4vRA39ciKwKd0WC7icIeO6q9ucKvegrQCr1vaEQR1f9lvnHdmWO1ob9bftL4eQWhzmtMmkTcNjp2Rotm4rAd8h_hHrKcgYemiAjDCS6MfhSgj-_b6IB2AkOdqKgS0YRCTlS9NBu4SXkL-v4VISfOqXE6irJexAnktB3akYBdPKhSeC3eK5-d-DhYxk9EebE9DRPSH14nTZG81HU2yZKy-DzTUr4XAWM9sLdgEAXB0MfVXS4gVN7dykr-5W76Qn9XndsllakGjzOdnTWb2gV6Qe-p3nf-6IlkiQP0FO3zpdpd7f21OBz7uilmQIPHXD4pHGy5uP0rxNLHsE6aC5DSt0DQ0Gk3iloEpXQwfwABrPM6mhDVimZ9DFA25vNZ9V-Rjr8K1KS3cOLvxg=w598-h341-no",
   },{
-    "photo": "assets/images/services-cover-custom-woodwork.png",
+    "photo": "https://lh3.googleusercontent.com/aVQVpXkAxpGUWVOCnN_MDH0cSzUoXBK4sWflVNzp4K1LisHqYNzmEWYHs4JHzmBHu7I4Kmx8AGT2lsEtMFkyui3UdqEv3OfGgogJYkeXseGpkespwniJxW-dMzN7Ike05_eZQSrksPN9qqPdsQZIfl1l0ktwOQYXnCNf36E8g5s-pMLZ132m_qjKE3B_5LbtSZwt3IDXYoLkD-s4MemOBQytHGEHpEJYCTk2LRjNW0pXti-j_2qNMzld0S-3gQsc56jBDESMvhqCKe_tCCeWCIMfef1vzQfrfptAJ4oTJGiXP5WVag8CEnTEBckHrHJqLVJedFNFhTxbCpEPSMf1zf51Pdc3sV49RDXH6Uhj1BiM8A4bVClgqrPcKWTo_1lTrqZaoydPglPt8zbewVlfrDQJfLGpyyqffRIScqo2JGnL8OWacjlZ4TW81ZpBRfvdCGzyaBU8yR_Y-kwRruX4-kD3cLK5fmi6HcFXAkwMnNz63JXa1PgI_mGfzNz3Y5YloB7KkvkwBMr7UV7E-WSrxpQDydqstwACANiKDZcuqbtt95EM10_-DgXQHnImqYYILVAMc4xf7Az7cS0B4rs3wlFyMNxRrGgTLkxVkg=w453-h803-no",
   },{
-    "photo": "assets/images/services-cover-custom-woodwork.png",
+    "photo": "https://lh3.googleusercontent.com/fIjy8I3f1j0o1UVFPWEm5q4rnfMM3gP2Jbl-G4_G6cJK_B34YHp-7CWYFJvQrt5vPbYY15qHdJV7tfCit4LgCWKO92bqVS0q9i9OiI2CXZzqX5Pen7eXonKKt1O3v1v-9pMhssE24GqYepxiovHJdL-J1yEz-xxsJ6NOiIKG5poJwdO1vjymp8QhNuoZk5Lhsi7BoZIyoiynQig7pGgvyDxX6eqEiLMk0vt3-i16dvajQ4EL_Pev14ctmUOe6Vbxd5NWW4-auN55bRjam9E35gn1RyKc_sd0h4pyM85gqDmioEed-ktXVglgw6mePNH6FXz4zDq-h7CrX8d_3DiXa-3c7rwr1Gk21YbgFcLPAeRZ4qa834jgsTnpgDrP-56Sa5zNLJXPwz_l5uYrbk72ZdvCBFnKlUByzjyl9BV1hOFSE01h-stk3ic_Ln5kR0pQIgYcSJEdckwz2tLEnp-G97s14rQ2mOxZtAOj0EGSabmGq1QDmt35Xpe8v0kVN5yhVUuftUn2_33Sp0gBhJY3mPxKr1GzbbS9wsLqpjfgzQHC_p9hrXU5hQuNXayQhmbZLgDFoKlDYMjKyt0c_rDODpLlGo1CR2DC0l98uQ=s200-no",
   },{
-    "photo": "assets/images/services-cover-custom-woodwork.png",
+    "photo": "https://lh3.googleusercontent.com/6rOYFTgcO0JjfZdZvUdwd3HqkPcRoRmy-IBljmxnSsvq9UVHN4gVVd4P6R-BhGVMdgxNrrwT93ZIeesPGRbNEPigJxv_MVb6uyVNB9sqq4W1D1CmwiZgz1QAdzfBIVNH4rT2UOsjc7JbZbAfTCA2tUXOyOsKYAo5fjLe_reBBpYk9b8UWyt159zeTNRSpxzSOyKnshmS3LZmcZU-83Sg_uWSLTjAowfQSVke2tm5e-r91GtbZYgxngrLtSpwryFZdXglTTiUq9QI-QmiVeiIjZ6NTK52zwDW3STLBlZ3cUqHZEgusuSru1_uU0U5SH-Po12ElvvfQb7hQMF8kehpQS-1tuvm6iNJXgrf2MEX__K9Ys9oUTgURn7yKHEpb53zaSNGYCrGabq8rIaLm-oTfvmqjlOmFb2lN6k832TS0PjzcKgEwpBcQaxAywjlMNPJkyMw4_OF8OYP35nIBkcHLFmQNx_DKN7yCxLs2Iu7qF6iu-enCzmNCtsaAeUcDAeTbiOIsWgQFfG5Ehu7hHYSFah_T6YdY9ZH-ELSdZ-5QOiGq-eOV3ohDrspCgUMUB1r_yUsyWSfIa7sFQ4HufujbjJmotbQR_uVKlq8Lw=s200-no",
   },{
-    "photo": "assets/images/services-cover-custom-woodwork.png",
-  },{
-    "photo": "assets/images/services-cover-custom-woodwork.png",
-  },{
-    "photo": "assets/images/services-cover-custom-woodwork.png",
-  },{
-    "photo": "assets/images/services-cover-custom-woodwork.png",
-  },
+    "photo": "https://lh3.googleusercontent.com/epvi701XrPWSirSq_CjgFV8rnDUAZK_2cvOZmp0fVoFCTBWYClipkEMpWYVLLPluC2jsCgiWuoPiyVyLVdHCHgdESZ6lX2YzT4fw5h8PNUoohiSdh-PAc07E0ksz0jnCPgyG3uAbls7oEM3b6BEf72aKIdFZFc4atcNf-Wf6jBmXBENNWjfNktUsPA4PBewu3YR7-8Nvs2hjHwBhgHDNnNaxO-rZBK6jO7pfS9mKXekwZ4M6YTJKNq-Iw2UsR2odTKhdX-46hhqYCn3iGKPvgyEipcbOnpy3UVDdOIYS2mtrw8E8uTp0kYC2GMOxuCvSEBdkFyR_znyEwSBvZVR3r0SSGOZEK064C7qXLBbREhQKksvZT1saQWiOwP8KGyITa28tOPes9eCZZ8S7rJmSL7C0kc9jylIkAQ6nkodsf7OiRemAWJujqn5d2TiWnbwoYFA01hj0aZYGikMz5WzgFTLkVB90BW_7xKcIOZdx9E3WPQqfopMLpoZagE87zc2fB4d7Vr4gqRyYNNhiap5UHv3gBa8-1kQE2JsyXrYHkQ21J_VfrBlQzrWx5THOAfZNZuRKSNSrjT5Blum3kJR8chpeeJh6h5qJn241gg=s200-no",
+  }
 ];
 
 $(document).ready(function(){
@@ -112,7 +70,7 @@ $(document).ready(function(){
   window.addEventListener("hashchange", shiftWindow);
 
   $.each(galleryPatio, function (key, data) {
-    $("#gallery-patio-container").append('<figure><div class="col s6 m3"><image class="materialboxed gallery-margin" width="100%" src="' + data['photo'] + '"></div></figure>');
+    $("#gallery-patio-container").append('<figure><div class="col s6 m3"><image class="materialboxed gallery-margin" width="100%" height="200px" src="' + data['photo'] + '"></div></figure>');
     });
 
   $.each(galleryStainless, function (key, data) {
